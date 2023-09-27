dd0648a9f85e1c38ff1f37d18530fc3065fc6cbb
Patterbear <benjamin.mcgregor2002@gmail.com>
2023-09-27 16:26:34 +0100

    add menu screen

--- src/HospiSys.java
+++ src/HospiSys.java
@@ -2,8 +2,6 @@ package src;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 // Main class
 public class HospiSys {
@@ -36,12 +34,7 @@ public class HospiSys {
 
         // login button
         JButton loginButton = new JButton("Login");
-        loginButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                login(usernameEntry.getText(), passwordEntry.getText(), frame);
-            }
-        });
+        loginButton.addActionListener(e -> login(usernameEntry.getText(), passwordEntry.getText(), frame));
 
         loginButtonPanel.add(loginButton);
 
@@ -69,7 +62,65 @@ public class HospiSys {
     // will open menu screen
     private static void menu(String user) {
         System.out.println("Logged in");
+
+        // menu window JFrame initialisation and configurations
+        JFrame frame = new JFrame("HospiSys - Menu");
+        frame.getContentPane().setLayout(new GridLayout(0, 1));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+
+        // frame panels
+        JPanel patientPanel = new JPanel();
+        JPanel staffPanel = new JPanel();
+        JPanel logoutButtonPanel = new JPanel();
+
+        // buttons
+        JButton patientSearch = new JButton("Search");
+        patientSearch.addActionListener(e -> search("Patient"));
+
+        JButton addNewPatient = new JButton("Add New");
+        addNewPatient.addActionListener(e -> addNew("Patient"));
+
+        JButton staffSearch = new JButton("Search");
+        staffSearch.addActionListener(e -> search("Staff"));
+
+        JButton addNewStaff = new JButton("Add New");
+        addNewStaff.addActionListener(e -> addNew("Staff"));
+
+        JButton logout = new JButton("Log Out");
+        logout.addActionListener(e -> {
+            frame.dispose();
+            start();
+        });
+
+        // add components to panels
+        patientPanel.add(new JLabel("Patients"));
+        patientPanel.add(patientSearch);
+        patientPanel.add(addNewPatient);
+
+        staffPanel.add(new JLabel("Staff"));
+        staffPanel.add(staffSearch);
+        staffPanel.add(addNewStaff);
+
+        logoutButtonPanel.add(logout);
+
+        // add panels to frame
+        frame.getContentPane().add(patientPanel);
+        frame.getContentPane().add(staffPanel);
+        frame.getContentPane().add(logoutButtonPanel);
+
+        frame.setVisible(true);
+
     }
+
+    private static void search(String category) {
+        System.out.println(category);
+    }
+
+    private static void addNew(String category) {
+        System.out.println(category);
+    }
+
 
     // main method
     public static void main(String[] args) {
