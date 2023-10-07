ff80676f8d5646551f60913b8eb038bdbcb70025
Patterbear <benjamin.mcgregor2002@gmail.com>
2023-10-07 20:06:55 +0100

    add additional playfair fundamentals

--- src/HospiSys.java
+++ src/HospiSys.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.Arrays;
 
-import static java.util.Collections.addAll;
 
 // Main class
 public class HospiSys {
@@ -54,17 +53,13 @@ public class HospiSys {
     // verifies login details and opens menu
     private static void login(String user, String password, JFrame frame) {
         System.out.println(user + password);
-
-        if(true) {
-            frame.dispose();
-            menu(user);
-        }
+        frame.dispose();
+        menu(user);
     }
 
     // Menu function
     // will open menu screen
     private static void menu(String user) {
-        System.out.println("Logged in");
 
         // menu window JFrame initialisation and configurations
         JFrame frame = new JFrame("HospiSys - Menu");
@@ -121,13 +116,27 @@ public class HospiSys {
     }
 
     private static void addNew(String category) {
-        System.out.println(category);
-        playfairEncrypt("Benjamin");
+        playfairEncrypt("Benjamin says hellos", "William");
     }
 
-    private static String playfairEncrypt(String s) {
-        char[] key = s.substring(0, 4).toUpperCase().replace("J", "I").toCharArray();
 
+    // Remove char array duplicates function
+    private static char[] removeDuplicates(char[] charArray) {
+
+        String result = "";
+
+        for (int i = 0; i < charArray.length; i++) {
+            if (!result.contains(String.valueOf(charArray[i]))) {
+                result += charArray[i];
+            }
+        }
+
+        return result.toCharArray();
+    }
+
+    // Generate letters grid function
+    private static char[][] generateGrid(char[] key) {
+        key = removeDuplicates(key);
         String letters = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
 
         for (int i = 0; i < key.length; i++) {
@@ -135,24 +144,144 @@ public class HospiSys {
         }
 
         char[] lettersArray = (new String(key) + letters).toCharArray();
-        System.out.println(lettersArray);
 
-        char[][] letters2DArray = new char[5][5];
+        char[][] lettersGrid = new char[5][5];
 
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
-                letters2DArray[i][j] = lettersArray[(i * 5) + j];
+                lettersGrid[i][j] = lettersArray[(i * 5) + j];
             }
         }
 
-        System.out.println(Arrays.deepToString(letters2DArray));
+        return lettersGrid;
+    }
 
+
+    // Letters grid search function
+    // returns coordinates of given character in grid
+    private static int[] searchGrid(char[][] grid, char c) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < 5; j++) {
+                if(grid[i][j] == c) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+
+    // Print letters grid function
+    private static void printGrid(char[][] grid) {
+        String output = "";
+        for (int i = 0; i < grid.length; i++) {
+            output += Arrays.toString(grid[i]) + "\n";
+        }
+
+        System.out.println(output.substring(0, output.length() - 1)); // removes final new line
+    }
+
+
+    // Playfair string formatting function
+    private static String playfairFormatString(String s) {
+        if(s.length() % 2 != 0) {s+=" ";};
+        String output = "";
+
+        for (int i = 1; i < s.length(); i += 2) {
+            output += s.charAt(i - 1);
+            output += s.charAt(i);
+            output += " ";
+        }
+
+        return output.substring(0, output.length() - 1);
+
+    }
+
+
+    // Playfair encryption method
+    // returns cipher text created from given message and key
+    private static String playfairEncrypt(String s, String k) {
+
+        // format key
+        char[] key = k.toUpperCase().replace("J", "I").toCharArray();
+
+        // format message
+        String message = s.toUpperCase().replace(" ", "").replace("J", "I").toUpperCase();
+        System.out.println(message);
+
+        // handle duplicates
+        char[] messageChars = message.toCharArray();
+        String messageNoDuplicates = Character.toString(messageChars[0]);
+
+        for(int i = 1; i < message.length(); i++) {
+            if ((i + 1) % 2 == 0 && messageChars[i] == messageChars[i - 1]) {
+                messageNoDuplicates += "X";
+            }
+
+            messageNoDuplicates += messageChars[i];
+        }
+
+        // add 'Z' to end if odd number of letters
+        if (messageNoDuplicates.length() % 2 != 0) {
+            messageNoDuplicates += "Z";
+        }
+
+        System.out.println(playfairFormatString(messageNoDuplicates));
+
+        // generate grid and update message char array
+        char[][] lettersGrid = generateGrid(key);
+        messageChars = messageNoDuplicates.toCharArray();
+
+        // apply playfair rules
+        String result = "";
+
+        for (int i = 1; i < messageChars.length; i+=2) {
+            int[] first = searchGrid(lettersGrid, messageChars[i - 1]);
+            int[] second = searchGrid(lettersGrid, messageChars[i]);
+
+            // same row, shift right
+            if (first[0] == second[0]) {
+                if (first[1] == 4) {
+                    result += lettersGrid[first[0]][0]; // wraps around
+                } else {
+                    result += lettersGrid[first[0]][first[1] + 1];
+                }
+
+                if (second[1] == 4) {
+                    result += lettersGrid[second[0]][0]; // wraps around
+                } else {
+                    result += lettersGrid[second[0]][second[1] + 1];
+                }
+            }
+
+            // same column,shift down
+            else if(first[1] == second[1]) {
+                if (first[0] == 4) {
+                    result += lettersGrid[0][first[1]]; // wraps around
+                } else {
+                    result += lettersGrid[first[0] + 1][first[1]];
+                }
+
+                if (second[0] == 4) {
+                    result += lettersGrid[0][second[1]]; // wraps around
+                } else {
+                    result += lettersGrid[second[0] + 1][second[1]];
+                }
+            } else { // incomplete
+                result += messageChars[i - 1];
+                result += messageChars[i];
+            }
+        }
+
+
+        printGrid(lettersGrid);
+        System.out.println(playfairFormatString(result));
 
         return key.toString();
     }
 
 
-    // main method
+    // Main method
     public static void main(String[] args) {
         System.out.println("HospiSys is running...");
 
