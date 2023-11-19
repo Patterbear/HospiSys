18c4ba8f6f438fec9cf3ae2f2ea24c14126a352b
Patterbear <benjamin.mcgregor2002@gmail.com>
2023-11-19 18:15:52 +0000

    fix new user creation interface

--- src/HospiSysAdmin.java
+++ src/HospiSysAdmin.java
@@ -1,5 +1,6 @@
 package src;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
@@ -50,51 +51,82 @@ public class HospiSysAdmin {
 
         // new user button
         JButton newUserButton = new JButton("Create New User");
-        newUserButton.addActionListener(e -> newUser());
+        newUserButton.addActionListener(e -> {
+            try {
+                newUser();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
 
         frame.add(newUserButton);
 
         frame.setVisible(true);
     }
 
+
     // New user screen
     // allows user to create an account
     // allows assignment of admin status
-    private static void newUser() {
-
+    private static void newUser() throws IOException {
         // Frame setup
-        JFrame frame = new JFrame("HospiSys - Add User");
+        JFrame frame = new JFrame("HospiSys - Create new user");
         frame.setLocationRelativeTo(null);
-        frame.setLayout(new GridLayout(0, 1));
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setSize(480, 450);
+        frame.setSize(640, 275);
         frame.setIconImage(new ImageIcon("img/logo.png").getImage());
 
+        // logo
+        JLabel logo = new JLabel(new ImageIcon(ImageIO.read(new File("img/logo.png")).getScaledInstance(200, 150, Image.SCALE_FAST)));
+
+        // label
+        JLabel label = new JLabel("Create admin account:");
+        //label.setFont(font);
+
+        // entries section panel
+        JPanel entriesPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints entriesGbc = new GridBagConstraints();
+
         // Username entry
-        JPanel usernamePanel = new JPanel();
         TextField usernameEntry = new TextField(20);
-        usernamePanel.add(new JLabel("Username: "));
-        usernamePanel.add(usernameEntry);
+        entriesGbc.gridx = 0;
+        entriesGbc.gridy = 0;
+        entriesPanel.add(new JLabel("Username:"), entriesGbc);
+        entriesGbc.gridx = 1;
+        entriesGbc.anchor = GridBagConstraints.WEST;
+        entriesPanel.add(usernameEntry, entriesGbc);
 
         // Password entry
-        JPanel passwordPanel = new JPanel();
         TextField passwordEntry = new TextField(20);
         passwordEntry.setEchoChar('*');
         JButton viewPassword = new JButton("Show");
         viewPassword.addActionListener(e -> passwordEntry.setEchoChar((char)0));
-        passwordPanel.add(new JLabel("Password: "));
-        passwordPanel.add(passwordEntry);
-        passwordPanel.add(viewPassword);
+        entriesGbc.gridx = 0;
+        entriesGbc.gridy = 1;
+        entriesGbc.anchor = GridBagConstraints.CENTER;
+        entriesPanel.add(new JLabel("Password:"), entriesGbc);
+        entriesGbc.gridx = 1;
+        entriesGbc.anchor = GridBagConstraints.WEST;
+        entriesPanel.add(passwordEntry, entriesGbc);
+        entriesGbc.gridx = 2;
+        entriesPanel.add(viewPassword, entriesGbc);
 
         // Repeat password entry
-        JPanel repeatPasswordPanel = new JPanel();
         TextField repeatPasswordEntry = new TextField(20);
         repeatPasswordEntry.setEchoChar('*');
         JButton viewRepeatPassword = new JButton("Show");
         viewRepeatPassword.addActionListener(e -> repeatPasswordEntry.setEchoChar((char)0));
-        passwordPanel.add(new JLabel("Repeat password: "));
-        passwordPanel.add(repeatPasswordEntry);
-        passwordPanel.add(viewRepeatPassword);
+        entriesGbc.gridx = 0;
+        entriesGbc.gridy = 2;
+        entriesGbc.anchor = GridBagConstraints.CENTER;
+        entriesPanel.add(new JLabel("Repeat Password:"), entriesGbc);
+        entriesGbc.gridx = 1;
+        entriesGbc.anchor = GridBagConstraints.WEST;
+        entriesPanel.add(repeatPasswordEntry, entriesGbc);
+        entriesGbc.gridx = 2;
+        entriesPanel.add(viewRepeatPassword, entriesGbc);
 
         // Admin status panel
         JPanel makeAdminPanel = new JPanel();
@@ -102,7 +134,10 @@ public class HospiSysAdmin {
         JCheckBox adminStatus = new JCheckBox();
         makeAdminPanel.add(adminStatus);
 
+
+        // save button
         JButton saveButton = new JButton("Save");
+        //saveButton.setFont(font);
         saveButton.addActionListener(e -> {
             if (passwordEntry.getText().equals(repeatPasswordEntry.getText())) {
                 try {
@@ -126,11 +161,37 @@ public class HospiSysAdmin {
 
         });
 
-        frame.getContentPane().add(usernamePanel);
-        frame.getContentPane().add(passwordPanel);
-        frame.getContentPane().add(repeatPasswordPanel);
-        frame.getContentPane().add(makeAdminPanel);
-        frame.getContentPane().add(saveButton);
+        // add logo
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridheight = 4;
+        frame.getContentPane().add(logo, gbc);
+
+        // add label
+        gbc.gridx = 1;
+        gbc.gridheight = 1;
+        gbc.gridwidth = 3;
+        frame.getContentPane().add(label, gbc);
+
+        // add entries panel
+        gbc.gridy = 1;
+        gbc.gridheight = 3;
+        gbc.gridwidth = 3;
+        frame.getContentPane().add(entriesPanel, gbc);
+
+        // add admin status section
+        gbc.gridy = 4;
+        gbc.gridx = 1;
+        gbc.gridwidth = 3;
+        gbc.gridheight = 1;
+        frame.getContentPane().add(makeAdminPanel, gbc);
+
+        // add save button
+        gbc.gridy = 5;
+        gbc.gridx = 3;
+        gbc.gridwidth = 1;
+        //gbc.gridheight = 1;
+        frame.getContentPane().add(saveButton, gbc);
 
         frame.setVisible(true);
     }
